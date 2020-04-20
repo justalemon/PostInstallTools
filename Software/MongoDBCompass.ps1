4c0294c395e99dffea2ce6d006a40ddb67f59b96
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-20 06:52:46 -0400

    Added space on the MongoDB Compass name

--- Software/MongoDBCompass.ps1
+++ Software/MongoDBCompass.ps1
@@ -2,7 +2,7 @@
 # GUI for managing MongoDB instances
 
 $arguments = @{
-    Name = "MongoDBCompass"
+    Name = "MongoDB Compass"
     Uri = "https://downloads.mongodb.com/compass/mongodb-compass-community-1.20.5-win32-x64.msi"
     IsMSI = $true
 }
