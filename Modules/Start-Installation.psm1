7ace5547de000b31fc6b2a804f5118c94eac8bdd
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-20 06:15:08 -0400

    Added some logging during the installation

--- Modules/Start-Installation.psm1
+++ Modules/Start-Installation.psm1
@@ -25,6 +25,9 @@ function Start-Installation
         [switch]
         $IsMSI = $false
     )
+    # Log the name of the program that we are installing and do some tweaking
+    Write-Host "Starting installation of " -ForegroundColor Cyan -NoNewline
+    Write-Host $Name -ForegroundColor White
     $ProgressPreference = "SilentlyContinue"
 
     # Create the path that we are going to use
@@ -34,6 +37,11 @@ function Start-Installation
     # Remove the existing temp file if it exists
     Remove-Item -Path $file -Force -ErrorAction SilentlyContinue
     # Download the executable file and save it to %TEMP%
+    Write-Host "Downloading " -ForegroundColor Green -NoNewline
+    Write-Host $Uri -ForegroundColor White -NoNewline
+    Write-Host " to " -ForegroundColor Green -NoNewline
+    Write-Host $file -ForegroundColor White -NoNewline
+    Write-Host " - Please wait..." -ForegroundColor Green
     Invoke-WebRequest -Uri $Uri -OutFile $file
 
     # If this is an MSI, add the msiexec arguments and set the correct executable
@@ -49,11 +57,15 @@ function Start-Installation
     # Start the process with a clean Path and wait for it to finish
     # Sadly we need to do it like this for optional arguments
     # https://github.com/PowerShell/PowerShell/issues/4520 is not part of PS 5.1
+    Write-Host "Starting " -ForegroundColor Yellow -NoNewline
+    Write-Host "$Executable " -ForegroundColor White -NoNewline
     if ($Arguments)
     {
+        Write-Host $Arguments -ForegroundColor White
         Start-Process -FilePath $Executable -ArgumentList $Arguments -UseNewEnvironment -Wait
     }
     else {
+        Write-Host "" -ForegroundColor White
         Start-Process -FilePath $Executable -UseNewEnvironment -Wait
     }
 
@@ -63,5 +75,7 @@ function Start-Installation
     if ($StartKey -And $StartValue) {
         Remove-ItemProperty -Path $StartKey -Name $StartValue
     }
+
+    # Once everything is done, restore some of the tweaks
     $ProgressPreference = "Continue"
 }
