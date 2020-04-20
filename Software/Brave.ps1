fa65378ea33a8beb5c93ff20af93ed1c3307ba85
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-20 06:35:38 -0400

    Added script to install Brave

---
+++ Software/Brave.ps1
@@ -0,0 +1,8 @@
+# Brave Browser
+# A Light Chromium based Browser
+
+$arguments = @{
+    Name = "Brave"
+    Uri = "https://github.com/brave/brave-browser/releases/download/v1.7.92/BraveBrowserStandaloneSilentSetup.exe"
+}
+Start-Installation @arguments
